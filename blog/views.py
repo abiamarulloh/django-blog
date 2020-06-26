71f53de76525af9fe6a361c99f52d1f3b97b759e
abiamarulloh <abiamarulloh06@gmail.com>
2020-06-26 09:47:43 +0700

    comments

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from .models import Post
-from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
+from .models import Post, Comment
+from django.utils import timezone
+
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
@@ -46,3 +47,32 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+# Views Blog, tambah komentar
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # Jika form bernilai post 
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    # Jika tidak
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
